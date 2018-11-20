1e93f34535fdaa90e26d5de79d3aaeffb9043ac4
esosebee5 <esosebee114@gmail.com>
2018-11-20 12:08:20 -0500

    final? in sync w/ j & py

--- HelloBot_Cpp/HelloBot_Cpp.cpp
+++ HelloBot_Cpp/HelloBot_Cpp.cpp
@@ -40,6 +40,12 @@ struct Month
 Month getBirthMonth();
 /* Creates and returns a month struct based on the passed data */
 Month getMonth(string in);
+/* Returns a valid number for day of the month based on the number of days being passed in */
+int getBirthDay(int numberOfDays);
+/* Returns an integer year based on the input string */
+int getBirthYear(int currentYear);
+/* Shared method to handle string -> int conversion */
+void convertStringToIntIfValid(std::string &inputString, int &retVal);
 
 
 //+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -113,49 +119,68 @@ void clockApp()
 
 void calculateAge()
 {
-	Month birthMonth;
-	birthMonth = getBirthMonth();
-	int birthYear;
-	int birthDay;
-	
-	cout << "Enter your birth day (of the month): ";
-	cin >> birthDay;
-	cout << "Enter your birth year: ";
-	cin >> birthYear;
-
-	cout << "you entered: " << birthMonth.number << birthDay << birthYear;
-
-
+	startApp();
 
 	// get current d/t below:
 	tm *ltm = getCurrentDateTime();
-
-	// example of various components of tm structure:
-	cout << "Year: " << 1900 + ltm->tm_year << endl;
-	cout << "Month: " << 1 + ltm->tm_mon << endl;
-	cout << "Day: " << ltm->tm_mday << endl;
-	cout << "Time: " << ltm->tm_hour << ":";
-	cout << ltm->tm_min << ":";
-	cout << ltm->tm_sec << endl;
-
 	int currentYear = 1900 + ltm->tm_year;
 	int currentMonth = 1 + ltm->tm_mon;
 	int currentDay = ltm->tm_mday;
-	cout << currentYear << ", " << currentMonth << ", "
-		<< currentDay << "." << endl;
+
+	// get birth month
+	Month birthMonth;
+	birthMonth = getBirthMonth();
+	
+	// get birth day
+	int birthDay;
+	birthDay = getBirthDay(birthMonth.days);
+
+	// get birth year
+	int birthYear;
+	birthYear = getBirthYear(currentYear);
+
+	// Perform calculation
+	bool alreadyHadBirthday = 0;
+	int age = 0;
+	int monthsTill = 0;
+	int daysTill = 0;
+	if (currentMonth > birthMonth.number || (currentMonth == birthMonth.number && currentDay >= birthDay))
+	{
+		alreadyHadBirthday = 1;
+		age = currentYear - birthYear;
+	}
+	else 
+	{
+		age = currentYear - birthYear - 1;
+	}
+	////
+	//// logging
+	//cout << "Had birthday: " << alreadyHadBirthday << endl;
+	//cout << "age: " << age << endl;
+	////
+
+	cout << "You are " << age << " years old." << endl;
+	
+	endApp();
 }
 
 Month getBirthMonth()
 {
 	string monthInput;
-	cout << "Enter your birth month: ";
-	cin >> monthInput;
-	Month birthMonth = getMonth(monthInput);
+	string s;
+	Month birthMonth = { 0,0 };
 
-	// FOR LOGGING 
-	cout << "month number: " << birthMonth.number << endl;
-	cout << "days in month: " << birthMonth.days << endl;
-	// END LOGGING
+	while (birthMonth.days == 0)
+	{
+		cout << "Enter your birth month: ";
+		cin >> monthInput;
+		birthMonth = getMonth(monthInput);
+	}
+	
+	//// FOR LOGGING 
+	//cout << "month number: " << birthMonth.number << endl;
+	//cout << "days in month: " << birthMonth.days << endl;
+	//// END LOGGING
 
 	return birthMonth;
 }
@@ -175,46 +200,115 @@ Month getMonth(string in)
 	}
 	else if (in._Equal("mar") || in._Equal("march"))
 	{
-		month = { 1, 31 };
+		month = { 3, 31 };
 	}
 	else if (in._Equal("apr") || in._Equal("april"))
 	{
-		month = { 1, 30 };
+		month = { 4, 30 };
 	}
 	else if (in._Equal("may"))
 	{
-		month = { 1, 31 };
+		month = { 5, 31 };
 	}
 	else if (in._Equal("jun") || in._Equal("june"))
 	{
-		month = { 1, 30 };
+		month = { 6, 30 };
 	}
 	else if (in._Equal("jul") || in._Equal("july"))
 	{
-		month = { 1, 31 };
+		month = { 7, 31 };
 	}
 	else if (in._Equal("aug") || in._Equal("august"))
 	{
-		month = { 1, 31 };
+		month = { 8, 31 };
 	}
 	else if (in._Equal("sep") || in._Equal("september"))
 	{
-		month = { 1, 30 };
+		month = { 9, 30 };
 	}
 	else if (in._Equal("oct") || in._Equal("october"))
 	{
-		month = { 1, 31 };
+		month = { 10, 31 };
 	}
 	else if (in._Equal("nov") || in._Equal("november"))
 	{
-		month = { 1, 30 };
+		month = { 11, 30 };
 	}
 	else if (in._Equal("dec") || in._Equal("december"))
 	{
-		month = { 1, 31 };
+		month = { 12, 31 };
+	}
+	else
+	{
+		month = { 0, 0 };
 	}
 
 	return month;
+}
+
+int getBirthDay(int numberOfDays)
+{
+	int day = 0;
+
+	while (day <= 0 || day > numberOfDays)
+	{
+		cout << "Enter the day of the month: ";
+
+		string inputString;
+		cin >> inputString;
+
+		convertStringToIntIfValid(inputString, day);
+
+	}
+	////
+	//// logging
+	//cout << "day: " << day << endl;
+	////
+
+	return day;
+}
+
+int getBirthYear(int currentYear)
+{
+	int year = 0;
+
+	while (year <= 0 || year > currentYear)
+	{
+		cout << "Enter your birth year: ";
+
+		string inputString;
+		cin >> inputString;
+
+		convertStringToIntIfValid(inputString, year);
+
+	}
+	////
+	//// logging
+	//cout << "year: " << year << endl;
+	////
+
+	return year;
+}
+
+void convertStringToIntIfValid(std::string &inputString, int &retVal)
+{
+	string validNums = "0123456789";
+	int validCount = 0;
+
+	for (int i = 0; i < inputString.size(); i++)
+	{
+		std::size_t found = validNums.find(inputString[i]);
+		if (found != std::string::npos)
+		{
+			validCount++;
+		}
+	}
+
+	if (validCount == inputString.length())
+	{
+		istringstream converter(inputString);
+		converter >> retVal;
+	}
 }
 
 
